c3e7d31f4ce9e62e503ae165b08d99a50bd8dcc0
Phil <phil.pichet@gmail.com>
2017-05-16 14:55:19 +0200

    Creating footer on desktop

---
+++ src/ObservationBundle/Resources/public/images/icones/OpenClassrooms - Workplace_files/HgfEGYIY4FW.js
@@ -0,0 +1,4 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["TQOm+"]); }
+
+__d('LitestandClassicLayoutController',['cx','CSS'],(function a(b,c,d,e,f,g,h){var i={conditionCardClass:function j(k){if(document.body)c('CSS').conditionClass(document.body,"_5vb_",k);}};f.exports=i;}),null);
+__d('FaceliftGridController',['cx','CSS'],(function a(b,c,d,e,f,g,h){var i={conditionClass:function j(k){if(document.body)c('CSS').conditionClass(document.body,"_2xk0",k);}};f.exports=i;}),null);
---
+++ src/ObservationBundle/Resources/public/images/icones/OpenClassrooms - Workplace_files/mkdJP_MNbs5.js
@@ -0,0 +1,4 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["6viEF"]); }
+
+__d('MercuryMessageRenderLocations',[],(function a(b,c,d,e,f,g){'use strict';var h={CHAT:'chat',MESSENGER:'messenger',PAGES:'pages'};f.exports=h;}),null);
+__d('MessengerReactionsEmojiConst',['EmojiFormat'],(function a(b,c,d,e,f,g){function h(i){return c('EmojiFormat').codeArrayToUnicode(i);}f.exports={LOVE:h([128525]),HAHA:h([128518]),WOW:h([128558]),SAD:h([128546]),ANGRY:h([128544]),THUMBS_UP:h([128077]),THUMBS_DOWN:h([128078])};}),null);
