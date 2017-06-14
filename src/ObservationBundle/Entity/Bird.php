89e30a9f2ab41b9c78aca9fd7ef3dca387b41b12
Phil <phil.pichet@gmail.com>
2017-06-14 09:51:57 +0200

    Remove sound button

--- src/ObservationBundle/Entity/Bird.php
+++ src/ObservationBundle/Entity/Bird.php
@@ -12,14 +12,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Bird
 {
-    /**
-     * @ORM\OneToOne(targetEntity="ObservationBundle\Entity\Fiche")
-     */
-    protected $fiche;
-    /**
-     * @ORM\OneToOne(targetEntity="ObservationBundle\Entity\Sound")
-     */
-    protected $sound;
 
 
     /**
@@ -137,6 +129,10 @@ class Bird
      */
     private $pictures;
 
+    /**
+     * @ORM\OneToOne(targetEntity="ObservationBundle\Entity\Fiche")
+     */
+    protected $fiche;
 
     /**
      * Constructor
@@ -649,30 +645,6 @@ class Bird
     public function setFiche(\ObservationBundle\Entity\Fiche $fiche = null)
     {
         $this->fiche = $fiche;
-
-        return $this;
-    }
-
-    /**
-     * Get sound
-     *
-     * @return \ObservationBundle\Entity\Sound
-     */
-    public function getSound()
-    {
-        return $this->sound;
-    }
-
-    /**
-     * Set sound
-     *
-     * @param \ObservationBundle\Entity\Sound $sound
-     *
-     * @return Bird
-     */
-    public function setSound(\ObservationBundle\Entity\Sound $sound = null)
-    {
-        $this->sound = $sound;
 
         return $this;
     }
--- src/ObservationBundle/Resources/public/js/play.bird.sound.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $('#bird_sound').click(function () {
-        $('#player')[0].play();
-    })
-})
