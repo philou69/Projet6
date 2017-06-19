8e540486a762caa85be48a2450c953de27e9701e
flostevebron <eravillesteve@gmail.com>
2017-06-19 15:51:03 +0200

    Update bugs

--- src/ObservationBundle/Entity/Bird.php
+++ src/ObservationBundle/Entity/Bird.php
@@ -13,6 +13,10 @@ use Doctrine\ORM\Mapping as ORM;
 class Bird
 {
     /**
+     * @ORM\OneToOne(targetEntity="ObservationBundle\Entity\Fiche")
+     */
+    protected $fiche;
+    /**
      * @var int
      *
      * @ORM\Column(name="id", type="integer")
@@ -132,14 +136,12 @@ class Bird
      * @ORM\Column(name="bec", type="string", length=255)
      */
     private $bec;
-
     /**
      * @var string
      *
      * @ORM\Column(name="plumage", type="string", length=255)
      */
     private $plumage;
-
     /**
      * @var string
      *
