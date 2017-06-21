a979737863b5564a0866305d8309102c402a6ad3
flostevebron <eravillesteve@gmail.com>
2017-06-21 16:35:35 +0200

    Modif fiches + selection

--- dist/src/ObservationBundle/Resources/public/js/AJAX/Desktop/bird.list.js
+++ dist/src/ObservationBundle/Resources/public/js/AJAX/Desktop/bird.list.js
@@ -3,28 +3,40 @@
 // Requetes AJAX sur la pagination et recherche des oiseaux
 // Appeller dans la vue Bird/Desktop/list.html.twig
 $(document).ready(function () {
-
-    $('#type').change(function () {
-        $('#search').val('');
-    });
-
+    // Récuperation des differents elements
+    var $birds = $('#birds');
+    var $search = $('#search');
+    var $addBirds = $('#add-birds');
+    var $colorBec = $('#color-bec');
+    var $colorPlumage = $('#color-plumage');
+    var $colorPatte = $('#color-patte');
     // Création de la variable page
     var page = 1;
+
     // Première requete ajax lors du chargement de la page
     $.ajax({
-        url: $('#birds').data('href'),
+        url: $birds.data('href'),
         dataType: 'html',
         success: function success(code_html, status) {
             _success(code_html);
         }
     });
+    // Fonction qui gerer la liste des parametres GET passer aux requetes
+    function getParameters() {
+        var searchParameter = $search.val() === '' ? '' : 'search=' + $search.val() + '&';
+        var becParameter = $colorBec.val() === '' ? '' : 'bec=' + $colorBec.val() + '&';
+        var patteParameter = $colorPatte.val() === '' ? '' : 'patte=' + $colorPatte.val() + '&';
+        var plumageParameter = $colorPlumage.val() === '' ? '' : 'plumage=' + $colorPlumage.val() + '&';
+        var parameters = '?' + searchParameter + becParameter + patteParameter + plumageParameter;
+        return parameters;
+    }
 
     // Requte Ajax lors du click sur le bouton add
-    $('#add-birds').on('click', function (event) {
+    $addBirds.on('click', function (event) {
         event.preventDefault();
         prepareRequete(false);
-        var url = $('#add-birds').data('href').replace("1", page);
-        var parameters = $('#search').val() === '' ? '' : '?search=' + $('#search').val();
+        var url = $addBirds.data('href').replace("1", page) + getParameters();
+
         $.ajax({
             url: url + parameters,
             dataType: 'html',
@@ -34,21 +46,28 @@ $(document).ready(function () {
         });
     });
     // Requete ajax lors d'une recherche
-    $('#search').on('keyup', function (event) {
+    $search.on('keyup', function (event) {
         prepareRequete(true);
-
-        //Test pour filtre multicritère avec insertion de parametre de route
-        var option = $('#type').val();
-        var optionColor = $('#plumage').val();
-        var urlType = $('#search').attr('data-href').replace('lbNom', option).replace('plumage', optionColor);
-        var url = urlType + '?search=' + $(this).val();
-
-        // var url = $(this).data('href') + '?search=' + $(this).val();
+        var url = $search.data('href') + getParameters();
         $.ajax({
             url: url,
             dataType: 'html',
             success: function success(code_html, status) {
-                $('#birds').empty();
+                $birds.empty();
+                _success(code_html);
+            }
+        });
+    });
+
+    // Event sur les select du filtre
+    $('select').on('change', function () {
+        prepareRequete(true);
+        var url = $birds.data('href') + getParameters();
+        $.ajax({
+            url: url,
+            dataType: 'html',
+            success: function success(code_html, status) {
+                $birds.empty();
                 _success(code_html);
             }
         });
@@ -60,8 +79,7 @@ $(document).ready(function () {
         $('.loader').removeAttr('hidden');
         // S'il s'agit d'une recherche on vide le contenue de contentBirds et on met page à 1
         if (isSearch) {
-            console.log('coucou');
-            $('#birds').empty();
+            $birds.empty();
             page = 1;
         }
     }
@@ -71,16 +89,16 @@ $(document).ready(function () {
         // on cache le loader
         $('.loader').attr('hidden', true);
         // On inserre le resultat dans la zone contentBirds
-        $('#birds').append(code_html);
+        $birds.append(code_html);
         // On récuperer la taille du resultat
         var numbers = Number($('.length').data('length'));
         // On comparse celui-ci à 20 et aux nombres d'oiseaux affiché s'ils correspondent celà signifie qu'il n'y a pas d'autres pages
         if (numbers <= 20 || numbers == $('.bird').length) {
             // On cache donc le bouton add
-            $('#add-birds').attr('hidden', true);
+            $addBirds.attr('hidden', true);
         } else {
             // Sinon on l'affiche
-            $('#add-birds').removeAttr('hidden');
+            $addBirds.removeAttr('hidden');
         }
         // On supprime le div comptenant la taille du resultat pour évité les doublons
         $('.length').remove();
@@ -88,25 +106,10 @@ $(document).ready(function () {
         // On incrimente la page pour le prochain add
         page++;
     }
+
     // listener sur les div des oiseaux pour généreer un ien au click
     $(document).on('click', '.bird', function (event) {
         window.document.location = $(this).data('href');
-    });
-
-    $(window).scroll(function () {
-
-        // On vérifie si le document fait au moins 2 fois la taille de l'ecran et si le haut du scroll est superieur à la taille de l'écran
-        if ($(window).height() * 1.5 < $(document).height() && $(window).scrollTop() > $(window).height()) {
-            // Dans ce cas on affiche le bouton up
-            $('#up').css('display', 'inline');
-        } else {
-            // Sinon on le cache
-            $('#up').css('display', 'none');
-        }
-    });
-    $('#up-button').on('click', function () {
-        // Si on clique sur le bouton up, on reviens tout en haut du document
-        $(window).scrollTop(0);
     });
 });
 //# sourceMappingURL=bird.list.js.map
