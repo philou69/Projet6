0219a1114771da5813b2df636abec30003510027
Phil <phil.pichet@gmail.com>
2017-07-05 10:03:37 +0200

    Cleaning

--- dist/web/bundles/observation/js/play.bird.sound.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-$(document).ready(function () {
-    $('#bird_sound').click(function () {
-        $('#player')[0].play();
-    });
-});
-//# sourceMappingURL=play.bird.sound.js.map
