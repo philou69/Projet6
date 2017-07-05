6f7e98515fad599c469e484762f27c5fd9b26406
flostevebron <eravillesteve@gmail.com>
2017-07-05 17:30:58 +0200

    Fixing

--- src/ObservationBundle/Controller/ObservationController.php
+++ src/ObservationBundle/Controller/ObservationController.php
@@ -108,6 +108,7 @@ class ObservationController extends Controller
     {
 
         $observation = new Observation();
+        $observation->setUser($this->getUser());
         $session = new Session();
         $session->set('getBird', false);
         $em = $this->getDoctrine()->getManager();
@@ -132,10 +133,10 @@ class ObservationController extends Controller
             $files = $form->get('files')->getData();
 
             foreach ($files as $file) {
-                dump($file);
                 $picture = new Picture();
                 $picture->setFile($file)
                     ->setObservation($observation);
+                $observation->addPicture($picture);
 
                 // Si l'observation est dejà validé, on ajoute l'oiseau à la photo.
                 if($observation->getValidated() === true){
@@ -143,6 +144,9 @@ class ObservationController extends Controller
                 }
                 $em->persist($picture);
             }
+            // Appel du listener pour naturaliste
+            $obsListener = $this->get('observation.event_listener');
+            $obsListener->obsOfNaturaliste($observation);
 
             $observation->setUser($this->getUser());
 
@@ -157,7 +161,6 @@ class ObservationController extends Controller
             return $this->redirectToRoute('observation_add');
         }
 
-//        $device = $this->get('mobile_detect.mobile_detector');
         if($device->isMobile() || $device->isTablet()){
             return $this->render(
             'ObservationBundle:Observation:Mobile/add.html.twig', array(
@@ -175,20 +178,16 @@ class ObservationController extends Controller
     public function validateAction(Observation $observation)
     {
         // Action simple de mise à jour de l'entity sans vue renvoyant à la page de l'observation
-        // on passe l'observation en validé
-        $observation->setValidated(true)
-            ->setValidatedAt(new \DateTime())
-            ->setValidatedBy($this->getUser());
-        foreach ($observation->getPictures() as $picture)
-        {
-            $picture->setBird($observation->getBird());
+        // on s'assure que l'observation n'est pas déjà valider
+        if ($observation->getValidated() === false) {
+            $obsListener = $this->get('observation.event_listener');
+            $obsListener->validate($observation, $this->getUser());
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($observation);
+            $em->flush();
+            $this->get('event_dispatcher')->dispatch('observation.captured', new ObservationEvent($observation));
         }
-        $em = $this->getDoctrine()->getManager();
-
-        $em->persist($observation);
-        $em->flush();
-        $this->get('event_dispatcher')->dispatch('observation.captured', new ObservationEvent($observation));
-
         $this->addFlash('info', "L'observation a bien été enregistrée !");
 
         return $this->redirectToRoute('observation_view', array('id' => $observation->getId()));
@@ -201,18 +200,15 @@ class ObservationController extends Controller
     public function unvalideAction(Observation $observation)
     {
         // Action simple de mise à jour de l'entity sans vue renvoyant à la page de l'observation
-        // On va retirer l'oiseau des photos de l'observation
-        foreach ($observation->getPictures() as $picture) {
-            $picture->setBird(null);
+        // On vérifie si l'observation est validé
+
+        if ($observation->getValidated() === true) {
+            $obsListener = $this->get('observation.event_listener');
+            $obsListener->unvalidate($observation);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($observation);
+            $em->flush();
         }
-        $observation->setValidated(false)
-        ->setValidatedBy(null)
-        ->setValidatedAt(null);
-        $em = $this->getDoctrine()->getManager();
-
-        $em->persist($observation);
-        $em->flush();
-
         $this->addFlash('success', 'L\'observation a bien été invalidée');
         return $this->redirectToRoute('observation_view', array('id' => $observation->getId()));
 
